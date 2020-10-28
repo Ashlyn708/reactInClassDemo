135cbe899647a696b85ceaa30014ecb730dce7ef
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-10-28 01:12:38 +0000

    created demo

---
+++ react-demo/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+    let howMany = 3;
+ let apple= howMany == 1? "one": "too many";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        {apple}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
